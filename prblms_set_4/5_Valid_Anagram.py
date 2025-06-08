8f29955b08786c498ba421e636abe70feba89d4b
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-06-08 01:43:37 -0400

    Added solution for `Valid Anagram (Easy)`

---
+++ prblms_set_4/5_Valid_Anagram.py
@@ -0,0 +1,37 @@
+# 242. Valid Anagram (Easy)
+# https://leetcode.com/problems/valid-anagram
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True
+        
+
+"""
+✅ Time: O(n)
+✅ Space: O(1) → Because only 26 letters
+"""
+
+# Alternative Solution
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        freq = [0] * 26  # Only lowercase letters
+
+        for ch_s, ch_t in zip(s, t):
+            freq[ord(ch_s) - ord('a')] += 1
+            freq[ord(ch_t) - ord('a')] -= 1
+
+        return all(f == 0 for f in freq)
