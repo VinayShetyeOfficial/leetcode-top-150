089cc18188451e33ac5c5c6b199c2690500dd0ae
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-06-07 21:10:28 -0400

    Added solution for `Is Subsequence (Easy)`

---
+++ prblms_set_3/5_Is_Subsequence.py
@@ -0,0 +1,41 @@
+# 392. Is Subsequence (Easy)
+# https://leetcode.com/problems/is-subsequence
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        """
+        Returns True if s is a subsequence of t, else False.
+        """
+
+        s_ptr, t_ptr = 0, 0
+
+        # Traverse both strings
+        while s_ptr < len(s) and t_ptr < len(t):
+            if s[s_ptr] == t[t_ptr]:
+                s_ptr += 1
+            t_ptr += 1
+
+        return s_ptr == len(s)
+
+
+"""
+✅ Time Complexity: O(n)
+👉 n = length of t
+
+✅ Space Complexity: O(1)
+👉 Only two pointers used, no extra space
+"""
+
+# Alternate
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+
+        i = 0
+        for char in t:
+            if i < len(s) and char == s[i]:
+                i += 1
+
+        return i == len(s)
+        
