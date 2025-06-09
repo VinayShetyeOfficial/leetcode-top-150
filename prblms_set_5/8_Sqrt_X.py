1beb3bf73667ee7ccad77608040575573c8e6789
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-06-09 19:10:41 -0400

    Added solution for `Sqrt(x) (Easy)`

---
+++ prblms_set_5/8_Sqrt_X.py
@@ -0,0 +1,44 @@
+# 69. Sqrt(x) (Easy)
+# https://leetcode.com/problems/sqrtx
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+
+        left, right = 1, x // 2
+
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return right  # right is the floor of sqrt(x)
+
+"""
+✅ Time Complexity: O(log x)
+👉 Converges fast, logarithmic iterations
+
+✅ Space Complexity: O(1)
+👉 Constant space
+"""
+
+# Alternate
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        res = x
+
+        if x == 0:
+            return 0
+
+        while True:
+            res = (res + x / res) / 2
+
+            if round(res*res) == x:
+                break 
+                
+        return int(res)
