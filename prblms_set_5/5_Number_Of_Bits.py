f4ad35638c3ac6cf684b779b80c49997baf41190
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-06-09 19:10:16 -0400

    Added solution for `Number of 1 Bits (Easy)`

---
+++ prblms_set_5/5_Number_Of_Bits.py
@@ -0,0 +1,22 @@
+# 191. Number of 1 Bits (Easy)
+# https://leetcode.com/problems/number-of-1-bits
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        """
+        Returns the number of '1' bits (set bits) in the binary representation of n.
+        Uses Brian Kernighan's algorithm for efficient bit counting.
+        """
+        count = 0
+        while n:
+            n &= (n - 1)  # Clear the least significant set bit
+            count += 1
+        return count
+
+"""
+✅ Time Complexity: O(k)
+👉 k = number of set bits (more efficient than O(32) if few 1s)
+
+✅ Space Complexity: O(1)
+👉 Constant space
+"""
