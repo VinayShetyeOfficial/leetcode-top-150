9d3896d266a77574ff3ca8b45725a8161b2666f7
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-06-14 18:32:28 -0400

    Added solution for `Min Stack (Medium)`

---
+++ prblms_set_7/5_Min_Stack.py
@@ -0,0 +1,33 @@
+# 155. Min Stack (Medium) - Done
+# https://leetcode.com/problems/min-stack
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []      # Main stack to hold all values
+        self.minStack = []   # Auxiliary stack to track min at each level
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        # Push the new min value (either val or previous min)
+        minVal = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(minVal)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+"""
+✅ Time Complexity:
+All operations (push, pop, top, getMin) = O(1)
+
+✅ Space Complexity: O(n)
+We use an auxiliary stack (minStack) of same size as stack
+"""
