4e2de1940ba781e36b0e7884efdc0792ca73904c
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-06-22 18:48:52 -0400

    Added solution for `Factorial Trailing Zeroes (Medium)`

---
+++ prblms_set_8/5_Factorial_Trailing_Zeroes.py
@@ -0,0 +1,16 @@
+# 172. Factorial Trailing Zeroes (Medium)
+# https://leetcode.com/problems/factorial-trailing-zeroes
+
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+        while n >= 5:
+            n //= 5
+            count += n
+        return count
+
+
+"""
+✅ Time Complexity: O(log₅ n)
+✅ Space Complexity: O(1)
+"""
