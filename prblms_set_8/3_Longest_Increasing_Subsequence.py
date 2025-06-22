5c7f8fe2c43047fddcde1e546237a0a3a1f7d525
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-06-22 18:48:24 -0400

    Added solution for `Longest Increasing Subsequence (Medium)`

---
+++ prblms_set_8/3_Longest_Increasing_Subsequence.py
@@ -0,0 +1,24 @@
+# 300. Longest Increasing Subsequence (Medium)
+# https://leetcode.com/problems/longest-increasing-subsequence
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        LIS = [1] * len(nums)  # Every number alone is a subsequence of length 1
+
+        # Start from end and move to start
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    # We can include nums[i] before nums[j]
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+
+        return max(LIS)
+
+
+"""
+✅ Time Complexity: O(n^2)
+Two nested loops: one for each i, and one for each j > i
+
+✅ Space Complexity: O(n)
+For the LIS array
+"""
