794f6ec497a4eb9f9c59f489c331080e2b2d6793
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-09 21:28:25 -0400

    add `Intersection of Two Arrays II`

---
+++ blind-75-leetcode/29. Intersection of Two Arrays II.py
@@ -0,0 +1,42 @@
+# Link: https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+
+#  Intersection of Two Arrays II
+# ⭐ Solution1
+from collections import Counter
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        count1 = Counter(nums1)
+        count2 = Counter(nums2)
+        
+        intersection = []
+        for num in count1:
+            if num in count2:
+                intersection.extend([num] * min(count1[num], count2[num]))
+        
+        return intersection
+    
+# Driver code
+if __name__ == '__main__':
+    obj = Solution()
+    nums1 = [1,2]
+    nums2 = [1,1]
+    print(obj.intersect(nums1, nums2))
+
+# ----------------------
+
+# Another Solution
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        res = []
+        for i in nums1:
+            if i in nums2 and res.count(i) < nums2.count(i):
+                res.append(i)
+        return res
+        
+# Driver code
+if __name__ == '__main__':
+    obj = Solution()
+    nums1 = [4,9,5]
+    nums2 = [9,4,9,8,4]
+    print(obj.intersect(nums1, nums2))
