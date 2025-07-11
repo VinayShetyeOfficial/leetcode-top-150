36f6a733edf0dd68e0af3884bc7ce3d857cef4f8
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-10 20:23:14 -0400

    add `Find Smallest Letter Greater Than Target`

---
+++ blind-75-leetcode/42. Find Smallest Letter Greater Than Target.py
@@ -0,0 +1,52 @@
+# Link: https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
+
+# Find Smallest Letter Greater Than Target
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        charArr = letters
+        
+        for c in charArr:
+            if c > target:
+                return c
+        
+        return letters[0]
+            
+# Driver code
+if __name__ == '__main__':
+    obj = Solution()
+    
+    letters = ["c","f","j","b", "a","e", "z"]
+    target = "s"
+    print(obj.nextGreatestLetter(letters, target))
+
+
+# ----------------------
+
+# Another Solution
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        left, right = 0, len(letters) - 1
+        
+        # Edge case: If target is greater than or equal to the last character,
+        # wrap around to the first character
+        if target >= letters[right]:
+            return letters[0]
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if letters[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        # After binary search, 'left' will point to the smallest character greater than target
+        return letters[left]
+            
+# Driver code
+if __name__ == '__main__':
+    obj = Solution()
+    
+    letters = ["c","f","j","b", "a","e", "z"]
+    target = "s"
+    print(obj.nextGreatestLetter(letters, target))
+    
