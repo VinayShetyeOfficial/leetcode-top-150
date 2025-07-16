2971d60dab1338fa747ccff3a586db5e36a45d2a
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-16 00:53:21 -0400

    add `Largest Odd Number in a String`

---
+++ extras/TakeYouFoward/3_StringSeries/2_Largest_Odd_Number_in_a_String.py
@@ -0,0 +1,12 @@
+# Largest Odd Number in a String
+
+def largeOddNum(s: str) -> str:
+    for i in range(len(s) -1, -1, -1):
+        if int(s[i]) % 2 == 1:
+            return str(int(s[:i + 1]))  # int(ans) to remove prefixed zeros if any
+    return ""
+     
+if __name__ == '__main__':
+    s = "0032579"
+    ans = largeOddNum(s)
+    print(f'Lagest Odd Number Posssible: {ans}')
