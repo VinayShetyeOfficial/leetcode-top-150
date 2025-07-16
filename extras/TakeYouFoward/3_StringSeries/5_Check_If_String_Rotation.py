a8ce2e7f1e7a62c42914b17471589ba40bac67e2
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-16 00:53:53 -0400

    add `Check if string `s` can be rotated to form `goal` string`

---
+++ extras/TakeYouFoward/3_StringSeries/5_Check_If_String_Rotation.py
@@ -0,0 +1,18 @@
+# Check if string `s` can be rotated to form `goal` string
+
+def rotateString(s: str, goal: str) -> bool:
+    if len(s) != len(goal): return False
+    
+    for shift in range(len(s)):
+        rotated = s[shift:] + s[:shift]
+        if rotated == goal:
+            return True
+            
+    return False
+        
+if __name__ == '__main__':
+    s = "abcde" 
+    goal = "bcdea"
+    ans = rotateString(s, goal) 
+    print(f"The string `s` can become `goal` string? : {ans}")
+
