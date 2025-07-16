e40d481457c042ed606d0b838530f27f63511b8b
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-16 00:53:41 -0400

    add `Isomorphic String`

---
+++ extras/TakeYouFoward/3_StringSeries/4_Isomorphic_Strings.py
@@ -0,0 +1,19 @@
+# Isomorphic String
+
+def isomorphicString(s: str, t: str) -> bool:
+    s_to_t_map, t_to_s_map = {}, {}
+
+    for charS, charT in zip(s, t):
+        if (charS in s_to_t_map and s_to_t_map[charS] != charT) or \
+           (charT in t_to_s_map and t_to_s_map[charT] != charS):
+            return False
+        s_to_t_map[charS] = charT
+        t_to_s_map[charT] = charS
+
+    return True
+            
+if __name__ == '__main__':
+    s = "egg"
+    t = "add"
+    ans = isomorphicString(s, t) 
+    print(f"The two strings are {'not ' if not ans else ''}isomorphic.")
