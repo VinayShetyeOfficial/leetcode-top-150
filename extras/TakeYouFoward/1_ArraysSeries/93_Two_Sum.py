a26b40111b6f63d702493eb0a7b65c5fc5a07d3a
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-15 20:50:07 -0400

    add ` Two Sum`

---
+++ extras/TakeYouFoward/1_ArraysSeries/93_Two_Sum.py
@@ -0,0 +1,18 @@
+# Two Sum
+
+def twoSum(nums: list[int], target: int):
+    seen = {}
+    
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], index]
+        seen[num] = index
+        
+    return -1
+        
+if __name__ == "__main__":
+    nums = [1, 6, 2, 10, 3]
+    target = 0
+    ans = twoSum(nums, target)
+    print(f'Indices: {ans}')
