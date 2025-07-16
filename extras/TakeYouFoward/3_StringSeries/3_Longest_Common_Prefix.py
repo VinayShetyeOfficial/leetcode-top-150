b9dd791ab6ed2613c2243b3855c421500b618a1d
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-16 00:53:31 -0400

    add `Longest Common Prefix`

---
+++ extras/TakeYouFoward/3_StringSeries/3_Longest_Common_Prefix.py
@@ -0,0 +1,16 @@
+# Longest Common Prefix
+
+def longestCommonPrefix(strings: list[str]) -> str:
+    for i in range(len(strings[0])):
+        currentChar = strings[0][i]
+
+        for word in strings[1:]:
+            if word[i] != currentChar:
+                return strings[0][:i]
+            
+    return strings[0]
+            
+if __name__ == '__main__':
+    strings = ["interstellar", "internet", "internal", "intermediate", "intercom", "interview"]
+    ans = longestCommonPrefix(strings) # Longest common prefix: intre
+    print(f'Longest common prefix: {ans}')
