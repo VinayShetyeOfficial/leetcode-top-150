5315364762779e2725641394b859ecd1bcfe7d2e
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-15 20:50:37 -0400

    add `Lower Bound`

---
+++ extras/TakeYouFoward/2_BinarySearch/1_Lower_Bound.py
@@ -0,0 +1,26 @@
+# Lower Bound
+
+# Note: We must return index of 1st number greater than or equal to target, else array size
+
+def lowerBound(nums: list[int], target: int) -> int:
+    low, high = 0, len(arr) - 1
+    ans = len(arr)
+
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if arr[mid] >= target:
+            ans = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return ans
+    # Note: If  no answer is found, we return size of the array, hence initially ans = len(arr)
+
+if __name__ == "__main__":
+    arr = [3, 5, 8, 15, 19]
+    target = 9
+    indx = lowerBound(arr, target)
+    print("The lower bound is the index:", indx)
+
