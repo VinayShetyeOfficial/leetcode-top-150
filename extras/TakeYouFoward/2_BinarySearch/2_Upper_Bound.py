7a34c839a37b552aa620637f5fd28a65859b65b8
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-15 20:50:45 -0400

    add `Upper Bound`

---
+++ extras/TakeYouFoward/2_BinarySearch/2_Upper_Bound.py
@@ -0,0 +1,25 @@
+# Upper Bound
+
+# Note: We must return index of 1st number greater than target, else array size
+
+def upperBound(arr, target):
+    low, high = 0, len(arr) - 1
+    ans = len(arr)
+    
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if arr[mid] > target:
+            ans = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+            
+    return ans 
+    # Note: If  no answer is found, we return size of the array, hence initially ans = len(arr)
+
+if __name__ == "__main__":
+    arr = [3, 5, 8, 9, 15, 19]
+    target = 9
+    indx = upperBound(arr, target)
+    print("The upper bound is the index:", indx)
