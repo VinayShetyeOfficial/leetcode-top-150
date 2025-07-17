0d3cb6c07a1c01382ecba68f604f51d234da449b
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-17 13:31:56 -0400

    add `Generate Binary Strings Without Consecutive 1s`

---
+++ extras/TakeYouFoward/5_RecursionSeries/4_Generate_Binary_Strings_Without_Consecutive_Ones.py
@@ -0,0 +1,19 @@
+# Generate Binary Strings Without Consecutive 1s
+
+def generateBinaryStrings(n: int) -> list[str]:
+    # Helper function to check for consecutive 1's
+    def has_consecutive_ones(n: int) -> bool:
+        return(n & (n >> 1)) != 0
+    
+    result = []
+    for i in range(2**n):
+        check_num  = bin(i)[2:].zfill(n)
+        if not has_consecutive_ones(i):
+            result.append(check_num)
+            
+    return result
+    
+if __name__ == '__main__':
+    num = 3
+    ans = generateBinaryStrings(num)
+    print(f'Result: {ans}')
