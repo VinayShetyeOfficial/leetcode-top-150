fddcd6fcde87c6032f42d4cbf030099e9743534e
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-17 13:32:13 -0400

    add `Implement Queue using Arrays`

---
+++ extras/TakeYouFoward/6_StackSeries/1_Implement_Queue_Using_Array.py
@@ -0,0 +1,47 @@
+# Implement Queue using Arrays
+
+class ArrayQueue:
+    def __init__(self):
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        if not self.isEmpty():
+            return self.queue.pop(0)
+        return None  # Optional: safe fallback
+
+    def peek(self) -> int:
+        if not self.isEmpty():
+            return self.queue[0]
+        return None  # Optional: safe fallback
+
+    def size(self) -> int:
+        return len(self.queue)
+
+    def isEmpty(self) -> bool:
+        return len(self.queue) == 0
+
+
+if __name__ == '__main__':
+    queue = ArrayQueue()
+    queue.push(10)
+    queue.push(20)
+    queue.push(30)
+    queue.push(40)
+    queue.push(50)
+
+    print(f'Queue: {queue.queue}')
+    print(f'Popped Element: {queue.pop()}')
+    print(f'Front Element: {queue.peek()}')
+    print(f'Queue Size: {queue.size()}')
+    print(f'Is Queue Empty: {queue.isEmpty()}')
+
+    print()
+
+    print(f'Queue: {queue.queue}')
+    print(f'Popped Element: {queue.pop()}')
+    print(f'Front Element: {queue.peek()}')
+    print(f'Queue Size: {queue.size()}')
+    print(f'Is Queue Empty: {queue.isEmpty()}')
