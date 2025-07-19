74be77f5172c1f05c88a0c8c6494be4a4c6c7777
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-18 22:41:02 -0400

    add `Next Greater Element`

---
+++ extras/TakeYouFoward/6_StackSeries/6_Next_Greater_Element.py
@@ -0,0 +1,23 @@
+# Next Greater Element
+
+def nextLargerElement(arr):
+    n = len(arr)
+    result = [-1] * n
+    
+    for index in range(n):  # include last element also
+        start = index + 1
+        
+        while start < n:
+            if arr[start] > arr[index]:
+                result[index] = arr[start]
+                break
+            start += 1
+            
+    return result
+
+
+# ----------- Test Driver Code -----------
+if __name__ == '__main__':
+    arr = [3, 10, 4, 2, 1, 2, 6, 1, 7, 2, 9]
+    ans = nextLargerElement(arr)
+    print(f'Next Greater Element Result:\n{ans}')
