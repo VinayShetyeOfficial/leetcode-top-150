f7b6a7efb8f17cc1ae10597e3d91703da5f958fb
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-29 09:33:54 -0400

    update

--- extras/TakeYouFoward/1_ArraysSeries/0_Largest_Element_in_an_Array.py
+++ extras/TakeYouFoward/1_ArraysSeries/0_Largest_Element_in_an_Array.py
@@ -1,12 +1,33 @@
 # Largest Element in an Array
 
+import random
+
+# Find Largest Element in Array
+def compute(nums: list[int]) -> int:
+    print(nums)
+    nums = set(nums)
+    largestNum = next(iter(nums))
+ 
+    for num in nums:
+        if num > largestNum:
+            lagestNum = num
+            
+    return largestNum
+
+if __name__ == '__main__':
+    arr = [random.randint(1, 100) for _ in range(10)]
+    ans = compute(arr)
+    print(ans)
+
+#===================================================
+# Error
 class Solution:
     def largestElement(self, nums):
         nums = set(nums)
         largestNumber = nums[0]
 
         for num in nums:
-            if num > largestNumber:
+            if num > largestNumber:        # TypeError: 'set' object is not subscriptable
                 largestNumber = num
 
         return largestNumber
