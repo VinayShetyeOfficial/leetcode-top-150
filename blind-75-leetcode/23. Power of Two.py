d9279085596e8b184ff024c847b9489857ae4286
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-30 15:23:47 -0400

    update: add explanation

--- blind-75-leetcode/23. Power of Two.py
+++ blind-75-leetcode/23. Power of Two.py
@@ -10,3 +10,25 @@ if __name__ == '__main__':
     obj = Solution()
     num = 16
     print(obj.isPowerOfTwo(num))
+
+
+'''
+Example:
+--------
+Take n = 8 → 1000
+Then n - 1 = 7 → 0111
+Now do:
+  1000   (8)
+& 0111   (7)
+= 0000
+✅ So result is 0 → which means n is a power of 2
+
+Example:
+--------
+❌ Now take n = 10 → 1010
+Then n - 1 = 9 → 1001
+Now:
+  1010
+& 1001
+= 1000   → Not 0 → So not a power of 2
+'''
