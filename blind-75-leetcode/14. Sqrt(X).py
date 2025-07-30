86a6755b5c465f5b25ad4adfdfa1b312cb257ffd
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-30 13:17:11 -0400

    update: add primary solution

--- blind-75-leetcode/14. Sqrt(X).py
+++ blind-75-leetcode/14. Sqrt(X).py
@@ -1,6 +1,28 @@
 # Link: https://leetcode.com/problems/sqrtx/
 
 # Square Root Of Number
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+
+        left, right = 1, x // 2
+        while left <= right:
+            mid = (left + right) // 2
+            sqProd = mid * mid
+            if sqProd == x:
+                return mid
+            elif sqProd < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return right
+    
+# ============================================
+
+# Alternate Solution
+
 class Solution(object):
     def sqrt_of_num(self, num):
         
