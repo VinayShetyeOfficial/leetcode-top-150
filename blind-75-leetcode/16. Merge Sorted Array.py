ba7318ee62fe7f1f45f115c9c8ddedc608f54c61
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-30 13:42:46 -0400

    update: add optimized solution

--- blind-75-leetcode/16. Merge Sorted Array.py
+++ blind-75-leetcode/16. Merge Sorted Array.py
@@ -1,6 +1,39 @@
 # Link: https://leetcode.com/problems/merge-sorted-array/description/
 
 # Merge Sorted Array
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # Initialize the pointers
+        # Satrt filling from back of nums1
+        i = m - 1 # Last valid element in nums1
+        j = n - 1 # Last element in nums2
+        k = m + n - 1 # Last index in nums1 
+
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+        # Add if any leftover elements in nums2
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+
+# Time Complexity: O(m + n)
+# Space Complexity: O(1)
+
+# ============================================
+
+# Alternate Solution   
+
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         nums1[m:] = nums2
