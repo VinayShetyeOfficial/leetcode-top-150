4a8ca594ab36fe125e4ad33e342515f6313bc92a
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-07-31 11:36:14 -0400

    update

--- blind-75-leetcode/43. Min Cost of Climbing Stairs.py
+++ blind-75-leetcode/43. Min Cost of Climbing Stairs.py
@@ -1,27 +1,7 @@
 # https://leetcode.com/problems/min-cost-climbing-stairs/description/
 
 # Min Cost of Climbing Stairs
-class Solution(object):
-    def minCostClimbingStairs(self, cost):
-        
-        for i in range(len(cost)-3, -1, -1):
-            cost[i] += min(cost[i+1], cost[i+2])
-        
-        print(cost)
-        
-        return min(cost[0], cost[1])
-           
-# Driver code
-if __name__ == '__main__':
-    obj = Solution()
-    
-    cost = [1,100,1,1,1,100,1,1,100,1]
-    print(obj.minCostClimbingStairs(cost))
 
-    
-# ----------------------
-
-# Another Solution
 class Solution(object):
     def minCostClimbingStairs(self, cost):
         if not cost:
@@ -42,3 +22,26 @@ if __name__ == '__main__':
     
     cost = [1,100,1,1,1,100,1,1,100,1]
     print(obj.minCostClimbingStairs(cost))
+
+# Another Solution
+
+# =======================================================
+
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        
+        for i in range(len(cost)-3, -1, -1):
+            cost[i] += min(cost[i+1], cost[i+2])
+        
+        print(cost)
+        
+        return min(cost[0], cost[1])
+           
+# Driver code
+if __name__ == '__main__':
+    obj = Solution()
+    
+    cost = [1,100,1,1,1,100,1,1,100,1]
+    print(obj.minCostClimbingStairs(cost))
+
+    
