1a3dbea6deccc2aa4a9c60e986b4407a894e8ade
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-08-01 20:58:35 -0400

    update add optimized solution

--- blind-75-leetcode/47. Squares of a Sorted Array.py
+++ blind-75-leetcode/47. Squares of a Sorted Array.py
@@ -1,6 +1,29 @@
 # Link: https://leetcode.com/problems/squares-of-a-sorted-array/description/
 
 # Squares of a Sorted Array
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n  # Final sorted squares
+        left, right = 0, n - 1
+        pos = n - 1  # Position to insert next largest square
+
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                result[pos] = nums[left] * nums[left]
+                left += 1
+            else:
+                result[pos] = nums[right] * nums[right]
+                right -= 1
+            pos -= 1
+
+        return result
+    
+# =======================================================
+
+# Alternate Solution
+
 class Solution(object):
     def sortedSquares(self, nums):
         
