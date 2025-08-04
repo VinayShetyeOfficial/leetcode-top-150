616641d1a4ae1852270cfdc49c031fe48e0934f1
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-08-03 23:59:34 -0400

    update: add link

--- extras/TakeYouFoward/1_ArraysSeries/8_Remove_Duplicates_From_Sorted_Array.py
+++ extras/TakeYouFoward/1_ArraysSeries/8_Remove_Duplicates_From_Sorted_Array.py
@@ -1,3 +1,5 @@
+# Link: https://takeuforward.org/plus/dsa/problems/remove-duplicates-from-sorted-array
+
 # Remove duplicates from sorted array
 
 def removeDuplicates(nums: list[int]) -> list[int]:
