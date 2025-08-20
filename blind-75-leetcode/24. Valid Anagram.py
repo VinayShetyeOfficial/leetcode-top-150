3f3ab10eba07ed8948384c2717b84fa052296c31
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-08-19 21:58:56 -0400

    update: add time-complexity & space-complexity

--- blind-75-leetcode/24. Valid Anagram.py
+++ blind-75-leetcode/24. Valid Anagram.py
@@ -1,7 +1,10 @@
 # Link: https://leetcode.com/problems/valid-anagram/description/
 
-# Valid Anagram
-class Solution(object):
+# ============================================
+# Solution 1: Using count() with set
+# ============================================
+
+class Solution1(object):
     def isAnagram(self, s, t):
         if len(s) != len(t):
             return False
@@ -11,44 +14,58 @@ class Solution(object):
                 return False
         return True
 
-if __name__ == '__main__':
-    obj = Solution()
-    s = "anagram"
-    t = "nagaram"
-    print(obj.isAnagram(s, t))
+"""
+Time Complexity: O(n * m) 
+    - n = length of s, m = unique characters in s (set(s)) 
+    - s.count(char) and t.count(char) both take O(n)
+Space Complexity: O(m)
+    - For storing unique characters in set(s)
+"""
 
-# ----------------------
+# ============================================
+# Solution 2: Sorting
+# ============================================
 
-# Another Solution
-class Solution(object):
+class Solution2(object):
     def isAnagram(self, s, t):
-        t_sorted = sorted(t)   # ['a', 'a', 'a', 'g', 'm', 'n', 'r'] 
-        s_sorted = sorted(s)   # ['a', 'a', 'a', 'g', 'm', 'n', 'r'] 
-        
-        return t_sorted == s_sorted
-        # return ''.join(sorted(s)) == ''.join(sorted(t))
+        return sorted(s) == sorted(t)
 
-        
-if __name__ == '__main__':
-    obj = Solution()
-    s = "anagram"
-    t = "nagaram"
-    print(obj.isAnagram(s, t))
+"""
+Time Complexity: O(n log n)
+    - Sorting both strings takes O(n log n) time
+Space Complexity: O(n)
+    - For storing sorted lists of s and t
+"""
 
-# ----------------------
+# ============================================
+# Solution 3: Using Counter / Hashmap
+# ============================================
 
-# Another Solution
 from collections import Counter
 
-class Solution(object):
+class Solution3(object):
     def isAnagram(self, s, t):
-        s = dict(Counter(s))   # {'a': 3, 'n': 1, 'g': 1, 'r': 1, 'm': 1}
-        t = dict(Counter(t))   # {'n': 1, 'a': 3, 'g': 1, 'r': 1, 'm': 1}
+        return Counter(s) == Counter(t)
 
-        return s == t          # True
-        
+"""
+Time Complexity: O(n)
+    - Counting characters in both strings takes O(n)
+Space Complexity: O(n)
+    - For storing character counts in Counter dictionaries
+"""
+
+# ============================================
+# Driver Code
+# ============================================
+
 if __name__ == '__main__':
-    obj = Solution()
     s = "anagram"
     t = "nagaram"
-    print(obj.isAnagram(s, t))
+
+    obj1 = Solution1()
+    obj2 = Solution2()
+    obj3 = Solution3()
+
+    print("Solution1:", obj1.isAnagram(s, t))
+    print("Solution2:", obj2.isAnagram(s, t))
+    print("Solution3:", obj3.isAnagram(s, t))
