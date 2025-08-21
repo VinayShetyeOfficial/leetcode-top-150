8b9b06089eab18161570cb1b14c4a0ccbe045ff4
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-08-20 21:06:48 -0400

    update: add time-complexity & space-complexity

--- blind-75-leetcode/38. Reverse Words in a String III.py
+++ blind-75-leetcode/38. Reverse Words in a String III.py
@@ -1,18 +1,24 @@
 # Link: https://leetcode.com/problems/reverse-words-in-a-string-iii/
 
+# ============================================
 # Reverse Words in a String III
+# ============================================
 class Solution(object):
     def reverseWords(self, s):
-        res = []
-        
-        res += s.split(" ")
-        
-        return " ".join([w[::-1] for w in res])
-    
-# Driver code
+        # Split the sentence into words
+        words = s.split(" ")
+        # Reverse each word and join them back
+        return " ".join([w[::-1] for w in words])
+
+"""
+Time Complexity: O(n) where n is the length of the string
+Space Complexity: O(n) for storing reversed words
+"""
+
+# ============================================
+# Driver Code
+# ============================================
 if __name__ == '__main__':
     obj = Solution()
     s = "Let's take LeetCode contest"
-    print(obj.reverseWords(s))
-
-
+    print(obj.reverseWords(s))  # Output: "s'teL ekat edoCteeL tsetnoc"
