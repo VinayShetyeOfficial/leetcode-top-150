158297a460a4b8641fef99f38fa1e2fad82c7d1d
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-08-20 21:06:45 -0400

    update: add time-complexity & space-complexity

--- blind-75-leetcode/37. Fibonacci Number.py
+++ blind-75-leetcode/37. Fibonacci Number.py
@@ -1,41 +1,49 @@
 # Link: https://leetcode.com/problems/fibonacci-number/description/
 
-# Fibonacci Number
-# ⭐ Solution 1
-class Solution(object):
+# ============================================
+# Solution 1: Iterative (Bottom-up)
+# ============================================
+class Solution1(object):
     def fib(self, n):
         res = []
         
-        for i in range(0, n+1):
+        for i in range(0, n + 1):
             if i == 0:
-                res += [0]
+                res.append(0)
             elif i == 1:
-                res += [1]
+                res.append(1)
             else:
-                res += [res[i-1] + res[i-2]]
+                res.append(res[i - 1] + res[i - 2])
                 
         return res[n]
-    
-# Driver code
-if __name__ == '__main__':
-    obj = Solution()
-    n = 9
-    print(obj.fib(n))
 
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
 
-# ----------------------
-
-# Another Solution
-class Solution(object):
+# ============================================
+# Solution 2: Recursive
+# ============================================
+class Solution2(object):
     def fib(self, n):
         if n < 2:
             return n
-        
-        return self.fib(n-1) + self.fib(n-2)   # Recusion
-    
-# Driver code
-if __name__ == '__main__':
-    obj = Solution()
-    n = 10
-    print(obj.fib(n))
+        return self.fib(n - 1) + self.fib(n - 2)
 
+"""
+Time Complexity: O(2^n)
+Space Complexity: O(n) recursion stack
+"""
+
+# ============================================
+# Driver Code
+# ============================================
+if __name__ == '__main__':
+    n1 = 9
+    obj1 = Solution1()
+    print("Fibonacci Number (Iterative) for n =", n1, "is", obj1.fib(n1))
+
+    n2 = 10
+    obj2 = Solution2()
+    print("Fibonacci Number (Recursive) for n =", n2, "is", obj2.fib(n2))
