c6fcd1cd746e342e73f631070f0793d0cd904bc0
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-08-25 21:46:31 -0400

    update: add time-complexity & space-complexity

--- blind-75-leetcode/substep-requirements/SquareRoot Of Number.py
+++ blind-75-leetcode/substep-requirements/SquareRoot Of Number.py
@@ -1,16 +1,23 @@
 # Square Root Of Number
 def sqrt_of_num(num):
-    
     res = num
-    
+
     while True:
-        res = ( res + num / res ) / 2
-        
-        if round(res*res) == num:
+        res = (res + num / res) / 2  # Newton-Raphson Method
+
+        if round(res * res) == num:
             break
-    
+
     return int(res)
-    
+
+
 # Driver code
 if __name__ == '__main__':
-    print(sqrt_of_num(36))
+    print(sqrt_of_num(36))  # ✅ Output: 6
+
+
+# -----------------------------------------
+# Time Complexity (TC): O(log(num)) 
+#   -> Because Newton-Raphson method converges very fast.
+# Space Complexity (SC): O(1) 
+#   -> Only a few variables are used.
