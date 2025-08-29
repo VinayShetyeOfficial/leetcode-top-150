bc066dcf7ffd32cfb6316a84fdbc6f8489f3d805
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-08-29 19:33:36 -0400

    update: add time-complexity & space-complexity

--- extras/TakeYouFoward/1_ArraysSeries/93_Two_Sum.py
+++ extras/TakeYouFoward/1_ArraysSeries/93_Two_Sum.py
@@ -12,9 +12,12 @@ def twoSum(nums: list[int], target: int):
         seen[num] = index
         
     return -1
-        
+
 if __name__ == "__main__":
     nums = [1, 6, 2, 10, 3]
     target = 16
     ans = twoSum(nums, target)
     print(f'Indices: {ans}')
+
+# Time Complexity (TC): O(N) -> single pass through the list
+# Space Complexity (SC): O(N) -> dictionary to store elements
