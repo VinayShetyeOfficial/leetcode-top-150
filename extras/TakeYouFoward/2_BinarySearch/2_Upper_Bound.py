808251ad70220d0048c129e5ac48cc6d71b2753f
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-08-30 22:43:03 -0400

    update: add time-complexity & space-complexity

--- extras/TakeYouFoward/2_BinarySearch/2_Upper_Bound.py
+++ extras/TakeYouFoward/2_BinarySearch/2_Upper_Bound.py
@@ -4,7 +4,7 @@
 
 # Note: We must return index of 1st number greater than target, else array size
 
-def upperBound(arr, target):
+def upperBound(arr: list[int], target: int) -> int:
     low, high = 0, len(arr) - 1
     ans = len(arr)
     
@@ -17,11 +17,17 @@ def upperBound(arr, target):
         else:
             low = mid + 1
             
-    return ans 
-    # Note: If  no answer is found, we return size of the array, hence initially ans = len(arr)
+    return ans
+    # Note: If no answer is found, we return size of the array, hence initially ans = len(arr)
 
 if __name__ == "__main__":
     arr = [3, 5, 8, 9, 15, 19]
     target = 9
     indx = upperBound(arr, target)
     print("The upper bound is the index:", indx)
+
+
+"""
+Time Complexity (TC): O(log N) -> binary search over the array
+Space Complexity (SC): O(1) -> constant extra space used
+"""
