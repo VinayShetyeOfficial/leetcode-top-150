5e032d3eca1db54cd9eb6f45197cec59d9523aec
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-08-30 22:43:35 -0400

    update: add time-complexity & space-complexity

--- extras/TakeYouFoward/3_StringSeries/3_Longest_Common_Prefix.py
+++ extras/TakeYouFoward/3_StringSeries/3_Longest_Common_Prefix.py
@@ -3,16 +3,25 @@
 # Longest Common Prefix
 
 def longestCommonPrefix(strings: list[str]) -> str:
+    if not strings:
+        return ""
+
     for i in range(len(strings[0])):
         currentChar = strings[0][i]
 
         for word in strings[1:]:
+            # Stop if current index is out of range or character mismatch
             if i >= len(word) or word[i] != currentChar:
                 return strings[0][:i]
             
-    return strings[0]
+    return strings[0]  # All characters matched in all strings
             
 if __name__ == '__main__':
     strings = ["interstellar", "internet", "internal", "intermediate", "intercom", "interview"]
-    ans = longestCommonPrefix(strings) # Longest common prefix: intre
-    print(f'Longest common prefix: {ans}')
+    ans = longestCommonPrefix(strings)
+    print(f'Longest common prefix: {ans}')  # Output: "inter"
+
+"""
+Time Complexity (TC): O(N * M) -> N: number of strings, M: length of first string (worst case)
+Space Complexity (SC): O(1) -> only using a few variables
+"""
