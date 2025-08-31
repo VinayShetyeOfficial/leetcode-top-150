ccdcef332ce26f8d1a7e4f30f4d9e39adba45d32
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-08-30 22:43:15 -0400

    update: add time-complexity & space-complexity

--- extras/TakeYouFoward/3_StringSeries/1_Reverse_Words_In_String.py
+++ extras/TakeYouFoward/3_StringSeries/1_Reverse_Words_In_String.py
@@ -1,13 +1,18 @@
 # Link: https://takeuforward.org/plus/dsa/problems/reverse-every-word-in-a-string
 
-# Reverse words in a given string / Palindrome Check
+# Reverse words in a given string
 
 def reverseWords(s: str) -> str:
-    words = s.split()
-    reversed_words = words[::-1]
-    return ' '.join(reversed_words)
+    words = s.split()          # Split string into words
+    reversed_words = words[::-1]  # Reverse the list of words
+    return ' '.join(reversed_words)  # Join back into a string
      
 if __name__ == '__main__':
     s = "welcome to the jungle"
     ans = reverseWords(s)
     print(f'Reversed words in String: {ans}')
+
+"""
+Time Complexity (TC): O(N) -> splitting, reversing, and joining are all O(N)
+Space Complexity (SC): O(N) -> storing the words list and reversed_words list
+"""
