e0ee75f961170c7a436f696e2cf493b59babec3f
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-08-31 20:59:35 -0400

    update: add time-complexity & space-complexity

--- extras/TakeYouFoward/3_StringSeries/8_Max_Nexting_Depth_Of_Parenthesis.py
+++ extras/TakeYouFoward/3_StringSeries/8_Max_Nexting_Depth_Of_Parenthesis.py
@@ -1,23 +1,22 @@
 # Link: https://takeuforward.org/plus/dsa/problems/maximum-nesting-depth-of-the-parentheses
 
-# Maximum Nesting Depth of the Parentheses
+# Maximum Nesting Depth of Parentheses in a string
 
 def maxDepth(s: str) -> int:
-    max_depth, current_depth = 0, 0
-    
+    max_depth = 0
+    current_depth = 0
+
     for char in s:
         if char == '(':
             current_depth += 1
             max_depth = max(max_depth, current_depth)
         elif char == ')':
             current_depth -= 1
-            
-    return max_depth
-    
 
-# Example usage:
+    return max_depth
+
+# Example usage
 if __name__ == '__main__':
     s = "(1+(2*3)+((8)/4))+1"
     ans = maxDepth(s)
-    print(f'Max Depth Of Parenthesis: {ans}')
-    
+    print(f'Maximum Nesting Depth of Parentheses: {ans}')  # Output: 3
