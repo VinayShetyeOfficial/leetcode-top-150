f99f4d9bdd4a22f6d78abf6cebd95ef818797225
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-09-01 22:27:35 -0400

    update: time-complexity & space-complexity

--- extras/TakeYouFoward/4_LinkedListsSeries/2_Finding_Length.py
+++ extras/TakeYouFoward/4_LinkedListsSeries/2_Finding_Length.py
@@ -1,5 +1,3 @@
-# Link: https://takeuforward.org/plus/dsa/problems/find-the-length-of-the-linked-list
-
 # Find Linked List Length
 
 class Node:
@@ -41,62 +39,25 @@ class LinkedList:
         return length
 
 
-        
 # Example usage
 if __name__ == '__main__':
     ll = LinkedList()
     ll.insert(10)
     ll.insert(20)
     ll.insert(30)
-    
-    # Find Linked List Length
 
-class Node:
-    def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
-
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    # INSERT NODE AT THE END
-    def insert(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-        else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
-
-    # PRINT LINKED LIST
-    def print_list(self):
-        current = self.head
-        while current:
-            print(current.data, end=' -> ')
-            current = current.next
-        print('None')
-
-    # FIND LENGTH OF THE LIST
-    def list_length(self):
-        length = 0
-        current = self.head
-        while current:
-            length += 1
-            current = current.next
-        return length
-
-        
-# Example usage
-if __name__ == '__main__':
-    ll = LinkedList()
-    ll.insert(10)
-    ll.insert(20)
-    ll.insert(30)
-    print(ll.listLength())
-
+    print("Length of Linked List:", ll.list_length())
     ll.print_list()
-    print(ll.listLength())
+    print("Length of Linked List:", ll.list_length())
+
+
+"""
+Time Complexity:
+- insert(data): O(n) in worst case (since we traverse till end)
+- print_list(): O(n)
+- list_length(): O(n)
+
+Space Complexity:
+- O(1) for traversal (only pointers used), 
+- O(n) overall for storing n nodes in the list
+"""
