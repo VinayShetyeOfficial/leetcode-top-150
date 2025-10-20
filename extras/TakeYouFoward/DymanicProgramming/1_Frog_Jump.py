364df0fa4bc7315ad6fd55d2749a0d16b8ad5749
VinayShetyeOfficial <100470361+VinayShetyeOfficial@users.noreply.github.com>
2025-10-20 13:27:20 -0400

    update: add solution

--- extras/TakeYouFoward/DymanicProgramming/1_Frog_Jump.py
+++ extras/TakeYouFoward/DymanicProgramming/1_Frog_Jump.py
@@ -2,6 +2,27 @@
 
 # Frog Jump
 
+from typing import List
+
+def frogJump(heights: list[int]) -> int:
+    n = len(heights)
+    if n == 1:
+        return 0
+    if n == 2:
+        return abs(heights[1] - heights[0])
+
+    dp = [0] * n
+    dp[0] = 0
+    dp[1] = abs(heights[1] - heights[0])
+
+    for i in range(2, n):
+        one_step = dp[i-1] + abs(heights[i] - heights[i-1])
+        two_step = dp[i-2] + abs(heights[i] - heights[i-2])
+        dp[i] = min(one_step, two_step)
+
+    return dp[-1]
+
+# Alternate Solution
 def frogJump(heights):
     if not heights:
         return 0
